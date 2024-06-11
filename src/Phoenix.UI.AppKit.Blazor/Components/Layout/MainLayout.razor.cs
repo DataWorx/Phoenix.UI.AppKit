26f919801c9f761a559220e97fd349e2e9e90acc
raisr <kuss.rene+github@gmail.com>
2024-06-11 23:36:49 +0200

    Added initial mud layout

---
+++ src/Phoenix.UI.AppKit.Blazor/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Phoenix.UI.AppKit.Blazor.Components.Layout
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        #region Fields
+
+        private bool drawerOpen = true; 
+        
+        #endregion
+
+        private void DrawerToggle()
+        {
+            drawerOpen = !drawerOpen;
+        }
+    }
+}
