10f6a870772a9722844477e13d40c5bbac41e4e9
raisr <kuss.rene+github@gmail.com>
2024-06-13 00:11:47 +0200

    Updated AppBar, Added CssClasses

---
+++ src/Phoenix.UI.AppKit.Blazor/Theme/CssClasses.cs
@@ -0,0 +1,8 @@
+﻿namespace Phoenix.UI.AppKit.Blazor.Theme;
+
+public class ThemeCss
+{
+    public static string PrimaryForeground = "orange-text text-darken-3";
+
+    public static string AddClasses(params string[] classes) => string.Join(" ", classes);
+}
